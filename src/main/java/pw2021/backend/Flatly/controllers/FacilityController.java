8d6a47b1cf608d2f4b391daa682f463999680442
Bartosz Błachut <bartoszek.blach@gmail.com>
2022-01-06 22:54:58 +0100

    added new functionality to facilities
    1. getting by name
    2. saving a new facility (only if a facility of the same name does not exist!)
    3. updating a facility
    4. deleting a facility
    
    Signed-off-by: Bartosz Błachut <bartoszek.blach@gmail.com>

--- src/main/java/pw2021/backend/Flatly/controllers/FacilityController.java
+++ src/main/java/pw2021/backend/Flatly/controllers/FacilityController.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 import pw2021.backend.Flatly.enities.Facility;
+import pw2021.backend.Flatly.enities.Flat;
+import pw2021.backend.Flatly.exceptions.BadRequestException;
+import pw2021.backend.Flatly.exceptions.NotFoundException;
 import pw2021.backend.Flatly.exceptions.UnauthorizedException;
+import pw2021.backend.Flatly.exceptions.UnprocessableEntityException;
 import pw2021.backend.Flatly.services.FacilityService;
 import pw2021.backend.Flatly.services.SecurityProvider;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "facilities")
@@ -26,5 +31,34 @@ public class FacilityController {
     public List<Facility> getFacilities(@RequestHeader HttpHeaders headers) throws UnauthorizedException {
         this.securityService.checkAuthenticatedAndMainAdmin(headers);
         return this.facilityService.getFacilities();
+    }
+
+    @GetMapping(path = "{name}")
+    public Facility getFacilityByName(
+            @RequestHeader HttpHeaders headers,
+            @PathVariable String name) throws UnauthorizedException, NotFoundException {
+        this.securityService.checkAuthenticatedAndMainAdmin(headers);
+        return this.facilityService.getFacilityByName(name);
+    }
+
+    @PostMapping
+    public Facility storeFacility(@RequestHeader HttpHeaders headers, @RequestBody Facility facility)
+            throws UnprocessableEntityException, UnauthorizedException, BadRequestException {
+        this.securityService.checkAuthenticatedAndMainAdmin(headers);
+        return this.facilityService.storeFacility(facility);
+    }
+
+    @PutMapping(path = "{facilityId}")
+    public Facility updateFacility(@RequestHeader HttpHeaders headers, @PathVariable long facilityId, @RequestBody Facility facility)
+            throws NotFoundException, UnprocessableEntityException, UnauthorizedException, BadRequestException {
+        this.securityService.checkAuthenticatedAndMainAdmin(headers);
+        return this.facilityService.updateFacility(facilityId, facility);
+    }
+
+    @DeleteMapping(path = "{facilityId}")
+    public String deleteFacility(@RequestHeader HttpHeaders headers, @PathVariable long facilityId)
+            throws NotFoundException, UnauthorizedException {
+        this.securityService.checkAuthenticatedAndMainAdmin(headers);
+        return this.facilityService.deleteFacility(facilityId);
     }
 }
--- src/main/java/pw2021/backend/Flatly/repositories/FacilityRepository.java
+++ src/main/java/pw2021/backend/Flatly/repositories/FacilityRepository.java
@@ -1,9 +1,16 @@
 package pw2021.backend.Flatly.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pw2021.backend.Flatly.enities.Facility;
 
+import java.util.Optional;
+
+import static org.hibernate.loader.Loader.SELECT;
+
 @Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
+    @Query("SELECT f FROM Facility f WHERE f.name = ?1")
+    Optional<Facility> getFacilityByName(String name);
 }
