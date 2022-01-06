532e7cb3a98b8d45308752e50783de2335d2b076
Bartosz Błachut <bartoszek.blach@gmail.com>
2022-01-06 22:54:58 +0100

    added new functionality to facilities
    1. getting by name
    2. saving a new facility (only if a facility of the same name does not exist!)
    3. updating a facility
    4. deleting a facility
    
    Signed-off-by: Bartosz Błachut <bartoszek.blach@gmail.com>

--- src/main/java/pw2021/backend/Flatly/services/FacilityService.java
+++ src/main/java/pw2021/backend/Flatly/services/FacilityService.java
@@ -3,6 +3,7 @@ package pw2021.backend.Flatly.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pw2021.backend.Flatly.enities.Facility;
+import pw2021.backend.Flatly.exceptions.BadRequestException;
 import pw2021.backend.Flatly.exceptions.NotFoundException;
 import pw2021.backend.Flatly.repositories.FacilityRepository;
 
@@ -30,5 +31,37 @@ public class FacilityService {
         }
 
         return facilityOptional.get();
+    }
+
+    public Facility getFacilityByName(String name) throws NotFoundException {
+        Optional<Facility> facilityOptional = this.facilityRepository.getFacilityByName(name);
+
+        if (facilityOptional.isEmpty())
+            throw new NotFoundException((String.format("Facility with name: %s does not exist", name)));
+
+        return facilityOptional.get();
+    }
+
+    public Facility storeFacility(Facility facility) throws BadRequestException {
+        try {
+            this.getFacilityByName(facility.getName());
+        } catch (NotFoundException e) {
+            // if facility is not found add it to the database
+            return this.facilityRepository.save(facility);
+        }
+
+        throw new BadRequestException(String.format("Facility with the name: %s already exists!", facility.getName()));
+    }
+
+    public Facility updateFacility(long facilityId, Facility facility) throws NotFoundException, BadRequestException {
+        Facility f = this.getFacility(facilityId);
+        f.setName(facility.getName());
+
+        return this.storeFacility(f);
+    }
+
+    public String deleteFacility(long facilityId) throws NotFoundException {
+        this.facilityRepository.delete(this.getFacility(facilityId));
+        return "deleted";
     }
 }
