f8c678c0b90abc778f7ce800ddadc2c3024f5d7d
RazorMeister <battnik90@gmail.com>
2022-01-31 14:11:48 +0100

    Added log

--- src/main/java/pw2021/backend/Flatly/repositories/FileSystemRepository.java
+++ src/main/java/pw2021/backend/Flatly/repositories/FileSystemRepository.java
@@ -1,14 +1,19 @@
 package pw2021.backend.Flatly.repositories;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.logging.Level;
 
 @Repository
 public class FileSystemRepository {
+    Logger logger = LoggerFactory.getLogger(FileSystemRepository.class);
+
     public String save(byte[] content, String imageName) throws Exception {
         String fileName = new Date().getTime() + "-" + imageName;
         Path newFile = Paths.get(
@@ -18,6 +23,8 @@ public class FileSystemRepository {
                 "static",
                 fileName
         );
+
+        this.logger.error(newFile.getParent().toString());
 
         Files.createDirectories(newFile.getParent());
 
